7b1a3287eae361221146343f7560fede072e1be9
carlos <carlosramos1434@gmail.com>
2018-02-28 11:22:06 -0300

    subindo primire comit

--- AulaExercicioMatriz3D27M02.c
+++ AulaExercicioMatriz3D27M02.c
@@ -1,35 +1,38 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 struct TAtleta{
 	int matriculaAt[50];
 	int idAt[3];
 	float alAt[5];
 };
 main(){
-	struct TAtleta AtletasCepe[15];
-	int i, cont;
-	float at, id;
+	struct TAtleta AtletasCepe[3];
+	int i , id=0, at=0, n, b;
 	
-	for(i=0; i<15 ;i++){
+	for(i=0; i<3 ;i++){
 		printf("informe o numero da matricula: ");
 			scanf("%d",&AtletasCepe[i].matriculaAt);
 		
 		printf("informe a idade: ");
 			scanf("%d",&AtletasCepe[i].idAt);
-			
+			id += AtletasCepe[i].idAt;
+		
 		printf("informe a altura: ");
-			scanf("%f",&AtletasCepe[i].alAt);
-		cont++;
-		
-		id =AtletasCepe[i].idAt / cont ;
-		at =AtletasCepe[i].alAt / cont ;
-		
+			scanf("%d",&AtletasCepe[i].alAt);
+			at += AtletasCepe[i].alAt;
+			
 	} 
-	for(i=0; i<15 i++){
-		printf("Numero da matricula: %d ",AtletasCepe[i].matriculaAt);
-		printf("Idade: %d ",AtletasCepe[i].idAt);
-		printf("Alturas: %f ",AtletasCepe[i].alAt);
+	n= id /3;
+	b = at /3;
+	
+	for(i=0; i<3 ;i++){
+		printf("Numero da matricula: %d \n",AtletasCepe[i].matriculaAt);
+		printf("Idade: %d \n",AtletasCepe[i].idAt);
+		printf("Alturas: %d \n",AtletasCepe[i].alAt);
 		
-		printf("Media da Alturas: %f ",at);
-		printf("Media das idades: %f ",id);
+		printf("Media da Alturas: %d \n",b);
+		printf("Media das idades: %d \n",n);
 	
 	}
 	
